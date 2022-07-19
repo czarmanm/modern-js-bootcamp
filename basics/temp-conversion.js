b02e5a80d2449d73cb6931b75ef5e949b2255dcb
César Mora <czarmanm@gmail.com>
2022-07-18 23:24:54 -0500

    Working with variables

---
+++ basics/temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheitValue = 32
+
+// calculate celsius and store in celsius variable
+let celsiusValue = (fahrenheitValue - 32) * 5 / 9
+// print that value
+console.log('Celsius value', celsiusValue)
+
+// calculate kelvin value and store in variable
+let kelvinValue = (fahrenheitValue - 459.67) * 5 / 9
+// print that value
+console.log('Kelvin value', kelvinValue)
